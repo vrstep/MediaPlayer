d47465cf78dce4d14bacfd106e4d4e2d77c1c358
Amir <amir.batyrggaliev@gmail.com>
2025-03-05 23:09:43 +0500

    Change playlist shape and carousel behaviour

--- app/src/main/java/kz/vrstep/mediaplayer/composables/PlaylistCarousel.kt
+++ app/src/main/java/kz/vrstep/mediaplayer/composables/PlaylistCarousel.kt
@@ -1,22 +1,34 @@
 package kz.vrstep.mediaplayer.composables
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun PlaylistCarousel() {
-    LazyColumn(modifier = Modifier.fillMaxWidth().height(200.dp)) {
+    LazyRow(modifier = Modifier.fillMaxWidth().height(150.dp)) {
         items(5) {
-            Card(shape = RoundedCornerShape(8.dp), modifier = Modifier.padding(8.dp).fillMaxWidth()) {
-                Text(text = "Playlist $it", modifier = Modifier.padding(16.dp))
+            Card(
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(120.dp)
+            ) {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Playlist $it")
+                }
             }
         }
     }
