779f7e4fbfae12f3e7b036498b280bdbf4303576
Amir <amir.batyrggaliev@gmail.com>
2025-04-02 22:46:19 +0500

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,4 +73,9 @@ dependencies {
 
     // For interop APIs with Material 3
     implementation("androidx.glance:glance-material3:1.1.0")
+
+    // Testing CI/CD
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
