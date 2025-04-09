3d6932b060f77b6cc240951016481eb54f34f5dc
Amir <amir.batyrggaliev@gmail.com>
2025-04-09 18:53:13 +0500

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     alias(libs.plugins.kotlin.compose)
 }
 
+val koin_android_version = "4.0.3"
+
 android {
     namespace = "kz.vrstep.mediaplayer"
     compileSdk = 35
@@ -78,4 +80,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Koin
+    implementation("io.insert-koin:koin-android:$koin_android_version")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 }
